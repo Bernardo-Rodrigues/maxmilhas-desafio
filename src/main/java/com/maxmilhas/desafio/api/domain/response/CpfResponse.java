fcf845b2982d0fd6abad7b188c9c45ef252f3720
Bernardo-Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2023-03-07 18:35:22 -0300

    fix: change swagger to open api dependency

--- src/main/java/com/maxmilhas/desafio/api/domain/response/CpfResponse.java
+++ src/main/java/com/maxmilhas/desafio/api/domain/response/CpfResponse.java
@@ -1,17 +1,11 @@
 package com.maxmilhas.desafio.api.domain.response;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
-@ApiModel("CpfResponse")
 public class CpfResponse {
-    @ApiModelProperty(name = "cpf", value = "Cpf")
     private String cpf;
-
-    @ApiModelProperty(name = "createdAt", value = "Data do registro")
     private LocalDateTime createdAt;
 }
